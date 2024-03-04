6381546c12f6949b2402d397dd0c61eb8bd647c2
Herick Ruan <152439267+HerickRuan@users.noreply.github.com>
2024-03-04 15:23:14 -0300

    update

--- scripts/script3.js
+++ scripts/script3.js
@@ -11,28 +11,46 @@ var pentagono = '../figuras/pentagono.png'
 var trapezio = '../figuras/trapezio.png'
 var triangulo = '../figuras/triangulo.png'
 
+var espaciais = document.querySelector(".espaciais")
+var planas = document.querySelector(".planas")
+
+
 
 
 function botaoP(){
     imain.style.backgroundColor = "var(--cor01)"
-    img1.src = circulo
-    img2.src = quadrado
-    img3.src = triangulo
-    img4.src = pentagono
-    img5.src = trapezio
-    img6.src = hexagono
+
+    img1.style.display = "inline-block"
+    img2.style.display = "inline-block"
+    img3.style.display = "inline-block"
+    img4.style.display = "inline-block"
+    img5.style.display = "inline-block"
+    img6.style.display = "inline-block"
+
+    img7.style.display = "none"
+    img8.style.display = "none"
+    img9.style.display = "none"
+    img10.style.display = "none"
+    img11.style.display = "none"
+    img12.style.display = "none"
+
 }
 
 function botaoE(){
     imain.style.backgroundColor = "var(--cor02)"
-    img1.src = esfera
-    img2.src = cubo
-    img3.src = piramide
-    img4.src = cilindro
-    img5.src = paralelepipedo
-    img6.src = cone
 
-    function calc1(){
-        window.alert("oi")
-    }
+    img1.style.display = "none"
+    img2.style.display = "none"
+    img3.style.display = "none"
+    img4.style.display = "none"
+    img5.style.display = "none"
+    img6.style.display = "none"
+
+    img7.style.display = "inline-block"
+    img8.style.display = "inline-block"
+    img9.style.display = "inline-block"
+    img10.style.display = "inline-block"
+    img11.style.display = "inline-block"
+    img12.style.display = "inline-block"
+
 }
